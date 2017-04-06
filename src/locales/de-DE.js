10770e25e652c509b246d7cb2d2c4eeaadd1baf1
Christian Blais <christ.blais@gmail.com>
2017-04-05 21:11:16 -0400

    Add German

---
+++ src/locales/de-DE.js
@@ -0,0 +1,193 @@
+if (typeof LANGUAGES === 'undefined') { var LANGUAGES = {} }
+
+LANGUAGES['de-DE'] = {
+  'general': {
+    'sections': {
+      'home': 'Übersicht',
+      'orders': 'Bestellungen',
+      'drafts': 'Entwürfe',
+      'checkouts': 'Warenkorbabbrecher',
+      'products': 'Produkte',
+      'customers': 'Kunden',
+      'reports': 'Berichte',
+      'discounts': 'Rabette',
+      'sales-channels': 'Verkaufskanäle',
+      'channels': 'Kanäle',
+      'online_store': 'Onlineshop',
+      'buy_button': 'Buy Button',
+      'mobile_app': "Mobile App",
+      'pos': "Point of Sale",
+      'apps': "Apps",
+      'settings': "Einstellungen",
+      'online_preferences': "Einstellungen",
+      'themes': "Themes",
+      'blogs': "Blogs",
+      'pages': "Seiten",
+      'navigation': "Navigation",
+      'domains': "Domains",
+      'inventory': "Lagerhaltung",
+      'inventory_transfers': "Lagerzugänge",
+      'collections': "Kollektionen",
+      'gift_cards': "Geschenkgutscheine",
+      'files': "Dateien",
+      'taxes': "Steuern",
+      'checkout': "Checkout",
+      'general': "Allgemein",
+      'payments': "Zahlungen",
+      'shipping': "Versand",
+      'notifications': "Benachrichtigungen",
+      'plan': "Tarif",
+      'account': "Konto",
+    },
+
+    'search': {
+      'placeholder': 'Suchen',
+    },
+
+    'buttons': {
+      'export': 'Exportieren',
+      'import': 'Importieren',
+      'save': 'Speichern',
+      'edit': 'Bearbeiten',
+      'cancel': 'Abbrechen',
+      'change': 'Ändern',
+      'destroy': "Löschen",
+      'post': 'Veröffentlichen',
+      'filter': 'Filtern'
+    }
+  },
+
+  'modal': {
+    'titles': {
+      'capture-payment': "Zahlung akzeptieren",
+    },
+  },
+
+  'home-index': {
+    'cards': {
+      'api_buy_button_create_first_embed_card': '',
+      'multi_image_buy_button_announcement_card': '',
+    },
+
+    'today-features': {
+      "todays-total-sales": "Heutige Umsätze",
+      "todays-visits": "Heutige Besuche",
+      "todays-orders": "Heutige Bestellungen",
+    },
+
+    'titles': {
+      'total-sales': "Umsätze",
+      'top-products': "Bestseller",
+    },
+
+    'buttons': {
+      'all-channels': "Alle Kanäle",
+      'online-store': "Onlineshop",
+      'view-order': "Bestellung ansehen",
+      'view-orders': "Bestellungen ansehen",
+      'create-button': "Button erstellen",
+      'create-buy-button': "Buy Button erstellen",
+      'learn-more': "Weitere Infos"
+    }
+  },
+
+  'products-index': {
+    'table': {
+      'tabs': {
+        'all-products': "Alle Produkte",
+      },
+    },
+
+    'buttons': {
+      'add-product': "Produkt hinzufügen",
+    },
+  },
+
+  'orders-index': {
+    'table': {
+      'tabs': {
+        'all-orders': "Alle Bestellungen",
+        'open': "Offen",
+        'unfulfilled': "Nicht ausgeliefert",
+        'unpaid': "Nicht bezahlt",
+      },
+
+      'columns': {
+        'order': 'Bestellung',
+        'date': 'Datum',
+        'customer': 'Kunde',
+        'payment-status': 'Zahlungsstatus',
+        'fulfillment-status': 'Auslieferungsstatus',
+        'total': 'Summe',
+      },
+
+      'badges': {
+        'fulfilled': 'Ausgeliefert',
+        'unfulfilled': 'Nicht ausgeliefert',
+        'paid': 'Bezahlt',
+        'pending': 'Wartend',
+        'refunded': 'Erstattet',
+        'authorized': "Autorisiert"
+      },
+    },
+
+    'filters': {
+      'status': "Status",
+      'credit-card': "Kreditkarte",
+      'customer': "Kunde",
+      'payment-status': "Zahlung",
+      'fulfillment-status': "Auslieferungsstatus",
+      'tagged-with': "Getaggt",
+      'risk-level': "Risikoklasse",
+    },
+
+    'buttons': {
+      'create-order': "Bestellung erstellen",
+    },
+  },
+
+  'orders-show': {
+    'buttons': {
+      'print': "Drucken",
+      'cancel-order': "Stornieren",
+      'archive': "Archivieren",
+      'view-order-status-page': "Bestellstatus-Seite",
+      'capture-payment': "Zahlung akzeptieren",
+      'restock': "Lagerbestand anpassen",
+      'fulfill-items': "Ausliefern",
+    },
+
+    'titles': {
+      'order-details': 'Details',
+      'unfulfilled': 'Nicht ausgeliefert',
+      'accept-payment': 'Zahlung',
+      'fulfill-items': 'Ausliefern',
+      'risk-level': 'Risikoklasse'
+    },
+  },
+
+  'customers-show': {
+    'titles': {
+      'recent-orders': "Letzte Bestellungen",
+      'default-address': "Standard-Adresse",
+      'contact': "Information",
+    },
+  },
+
+  'draft-orders-index': {
+    'buttons': {
+      'create-order': 'Bestellung erstellen'
+    },
+
+    'empty': {
+      'title': '',
+      'subtitle': ''
+    }
+  },
+
+  'draft': {
+    'buttons': {
+      'save-draft-order': 'Bestellung speichern'
+    }
+  },
+}
