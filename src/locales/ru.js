efe7c0f613cb19047d0bf85c9a58c043527f5f6a
Christian Blais <christ.blais@gmail.com>
2017-07-28 11:12:22 -0400

    Basic RU translations

---
+++ src/locales/ru.js
@@ -0,0 +1,204 @@
+if (typeof LANGUAGES === "undefined") { var LANGUAGES = {} }
+
+LANGUAGES["ru"] = {
+  "general": {
+    "sections": {
+      "home": "Главная",
+      "orders": "Заказы",
+      "drafts": "Черновики",
+      "checkouts": "Корзина",
+      "products": "Товары",
+      "customers": "Покупатели",
+      "reports": "Отчеты",
+      "discounts": "Скидки",
+      "sales-channels": "Каналы Продаж",
+      "channels": "Каналы",
+      "online_store": "Онлайн Магазин",
+      "buy_button": "Кнопка Купить",
+      "mobile_app": "Мобильное Приложение",
+      "pos": "POS-терминал",
+      "apps": "Приложения",
+      "settings": "Настройки",
+      "online_preferences": "Параметры",
+      "themes": "Шаблоны сайта",
+      "blogs": "Блоги",
+      "pages": "Страницы",
+      "navigation": "Навигация",
+      "domains": "Домены",
+      "inventory": "Складской учет",
+      "inventory_transfers": "Заказ поставщику",
+      "collections": "Коллекции",
+      "gift_cards": "Подарочные карты",
+      "files": "Архив",
+      "taxes": "Налоги",
+      "checkout": "Корзина",
+      "general": "Общие",
+      "payments": "Платежные системы",
+      "shipping": "Доставка",
+      "notifications": "Уведомления",
+      "plan": "Тариф",
+      "account": "Аккаунт",
+    },
+
+    "search": {
+      "placeholder": "Поиск",
+    },
+
+    "buttons": {
+      "export": "Экспортировать",
+      "import": "Импортировать",
+      "save": "Сохранить",
+      "edit": "Править",
+      "cancel": "Отменить",
+      "change": "Изменить",
+      "destroy": "Destruir",
+      "post": "Публиковать",
+      "filter": "Фильтр"
+    }
+  },
+
+  "modal": {
+    "titles": {
+      "capture-payment": "Оплата платежей",
+    },
+  },
+
+  "home-index": {
+    "cards": {
+      "api_buy_button_create_first_embed_card": "",
+      "multi_image_buy_button_announcement_card": "",
+    },
+
+    "today-features": {
+      "todays-total-sales": "Продажи сегодня",
+      "todays-visits": "Количество переходов сегодня",
+      "todays-orders": "Количество заказов сегодня",
+    },
+
+    "titles": {
+      "total-sales": "Общий объем продаж",
+      "top-products": "Лучшие товары",
+    },
+
+    "buttons": {
+      "all-channels": "Все каналы продаж",
+      "online-store": "Онлайн магазин",
+      "view-order": "Просмотреть заказ",
+      "view-orders": "Просмотреть заказы",
+      "create-button": "Создать кнопку",
+      "create-buy-button": "Создать кнопку корзины",
+      "learn-more": "Узнать больше"
+    }
+  },
+
+  "products-index": {
+    "table": {
+      "tabs": {
+        "all-products": "Все товары",
+      },
+    },
+
+    "buttons": {
+      "add-product": "Добавить товары",
+    },
+  },
+
+  "orders-index": {
+    "table": {
+      "tabs": {
+        "all-orders": "Все заказы",
+        "open": "Открыт",
+        "unfulfilled": "Ожидает обработки",
+        "unpaid": "Неоплачен",
+      },
+      "columns": {
+        "order": "Заказ",
+        "date": "Дата",
+        "customer": "Покупатель",
+        "payment-status": "Статус оплаты",
+        "fulfillment-status": "Статус отгрузки",
+        "total": "Итого",
+      },
+      "badges": {
+        "fulfilled": "Обработан",
+        "unfulfilled": "Ожидает обработки",
+        "paid": "Оплачено",
+        "pending": "Не обработан",
+        "refunded": "Возврат",
+        "authorized": "Утвержден"
+      },
+    },
+
+    "filters": {
+      "status": "Статус",
+      "credit-card": "Кредитная карта",
+      "customer": "Покупатель",
+      "payment-status": "Статус оплаты",
+      "fulfillment-status": "Статус отгрузки",
+      "tagged-with": "Теги",
+      "risk-level": "Уровень риска",
+    },
+
+    "buttons": {
+      "create-order": "Сделать заказ",
+    },
+  },
+
+  "orders-show": {
+    "buttons": {
+      "print": "Печать",
+      "cancel-order": "Отменить заказ",
+      "archive": "Архив",
+      "view-order-status-page": "Просмотр страницы заказов",
+      "capture-payment": "Оплата платежей",
+      "restock": "Пополнить",
+      "fulfill-items": "Реализовать",
+    },
+
+    "titles": {
+      "order-details": "Детали заказа",
+      "unfulfilled": "Ожидает обработки",
+      "accept-payment": "Принять оплату",
+      "fulfill-items": "Реализовать",
+      "risk-level": "Уровень риска"
+    },
+  },
+
+  "customers-show": {
+    "titles": {
+      "recent-orders": "Последние заказы",
+      "default-address": "Адрес по умолчанию",
+      "contact": "Контакты",
+    },
+  },
+
+  "draft-orders-index": {
+    "buttons": {
+      "create-order": "Создать заказ"
+    },
+    "empty": {
+      "title": "Наименование",
+      "subtitle": "Подзаголовок."
+    }
+  },
+
+  "draft": {
+    "buttons": {
+      "save-draft-order": "Сохранить черновик"
+    },
+
+    "titles": {
+      "order-details": "Детали заказов",
+      "conversion-details": "Детали конверсии",
+      "unfulfilled": "Ожидает Обработки",
+      "accept-payment": "Принять оплату",
+      "fulfill-items": "Реализовать",
+      "risk-level": "Уровень риска",
+      "customer": "Покупатель",
+      "tags": "Тэги",
+      "order-contact": "Контакты заказчика",
+      "timeline": "Сроки",
+      "payment-pending": "Оплата ожидается",
+    },
+  }
+}
