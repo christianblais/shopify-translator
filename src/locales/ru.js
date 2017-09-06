597ef0dea8c03cdb3729bac8beab5a4aadd79bf8
Christian Blais <christ.blais@gmail.com>
2017-09-06 04:59:08 -0400

    New translations fr.js (Russian)

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,204 +1,285 @@
-if (typeof LANGUAGES === "undefined") { var LANGUAGES = {} }
+if (typeof LANGUAGES === 'undefined') { var LANGUAGES = {} }
 
-LANGUAGES["ru"] = {
-  "general": {
-    "sections": {
-      "home": "Главная",
-      "orders": "Заказы",
-      "drafts": "Черновики",
-      "checkouts": "Корзина",
-      "products": "Товары",
-      "customers": "Покупатели",
-      "reports": "Отчеты",
-      "discounts": "Скидки",
-      "sales-channels": "Каналы Продаж",
-      "channels": "Каналы",
-      "online_store": "Онлайн Магазин",
-      "buy_button": "Кнопка Купить",
-      "mobile_app": "Мобильное Приложение",
-      "pos": "POS-терминал",
-      "apps": "Приложения",
-      "settings": "Настройки",
-      "online_preferences": "Параметры",
-      "themes": "Шаблоны сайта",
-      "blogs": "Блоги",
-      "pages": "Страницы",
-      "navigation": "Навигация",
-      "domains": "Домены",
-      "inventory": "Складской учет",
-      "inventory_transfers": "Заказ поставщику",
-      "collections": "Коллекции",
-      "gift_cards": "Подарочные карты",
-      "files": "Архив",
-      "taxes": "Налоги",
-      "checkout": "Корзина",
-      "general": "Общие",
-      "payments": "Платежные системы",
-      "shipping": "Доставка",
-      "notifications": "Уведомления",
-      "plan": "Тариф",
-      "account": "Аккаунт",
+LANGUAGES['fr'] = {
+  'general': {
+    'sections': {
+      'home': 'Tableau de bord',
+      'orders': 'Commandes',
+      'drafts': 'Pré-commandes',
+      'checkouts': 'Abandons',
+      'abandoned-checkouts': 'Checkouts abandonés',
+      'products': 'Produits',
+      'customers': 'Clients',
+      'reports': 'Rapports',
+      'discounts': 'Promotions',
+      'sales-channels': 'Canaux de vente',
+      'channels': 'Canaux de vente',
+      'online-store': 'Boutique en ligne',
+      'buy-button': 'Buy Button',
+      'mobile-app': "App mobile",
+      'pos': "Points de vente",
+      'apps': "Applications",
+      'settings': "Configuration",
+      'online_preferences': "Préférences",
+      'themes': "Thèmes",
+      'blogs': "Blogues",
+      'pages': "Pages",
+      'navigation': "Navigation",
+      'domains': "Domaines",
+      'inventory': "Inventaire",
+      'inventory_transfers': "Transferts",
+      'collections': "Collections",
+      'gift-cards': "Cartes Cadeaux",
+      'files': "Fichiers",
+      'taxes': "Taxes",
+      'checkout': "Paniers",
+      'general': "Général",
+      'payments': "Paiements",
+      'shipping': "Livraison",
+      'notifications': "Notifications",
+      'plan': "Profile",
+      'account': "Profile",
     },
 
-    "search": {
-      "placeholder": "Поиск",
+    'search': {
+      'placeholder': 'Recherche',
     },
 
-    "buttons": {
-      "export": "Экспортировать",
-      "import": "Импортировать",
-      "save": "Сохранить",
-      "edit": "Править",
-      "cancel": "Отменить",
-      "change": "Изменить",
-      "destroy": "Destruir",
-      "post": "Публиковать",
-      "filter": "Фильтр"
+    'account': {
+      'your-profile': 'Votre compte',
+      'log-out': 'Déconnexion',
+      'shopify-help-center': "Centre d'aide",
+      'community-forums': 'Forums',
+      'hire-a-shopify-expert': 'Engager un expert Shopify',
+      'keyboard-shortcuts': 'Raccourcis clavier',
+    },
+
+    'buttons': {
+      'export': 'Exporter',
+      'import': 'Importer',
+      'print': 'Imprimer',
+      'save': 'Sauvegarder',
+      'edit': 'Éditer',
+      'cancel': 'Annuler',
+      'change': 'Changer',
+      'destroy': "Détruire",
+      'post': 'Envoyer',
+      'filter': 'Filtrer'
     }
   },
 
-  "modal": {
-    "titles": {
-      "capture-payment": "Оплата платежей",
+  'modal': {
+    'titles': {
+      'capture-payment': "Capturer un paiement",
     },
   },
 
-  "home-index": {
-    "cards": {
-      "api_buy_button_create_first_embed_card": "",
-      "multi_image_buy_button_announcement_card": "",
+  'shared': {
+    'customer-card': {
+      'customer': 'Client',
+      'shipping-address': 'Adresse de livraison',
+      'billing-address': 'Adresse de facturation',
+      'no-address-set': 'Aucune adresse',
+      'same-as-shipping-address': "Identique à l'adresse de livraison",
+      'accepts-marketing': "Accepte le marketing",
+      'has-an-account': "Possède un compte",
+      'contact': "Contact",
+      'view-map': "Voir sur la carte",
     },
 
-    "today-features": {
-      "todays-total-sales": "Продажи сегодня",
-      "todays-visits": "Количество переходов сегодня",
-      "todays-orders": "Количество заказов сегодня",
-    },
-
-    "titles": {
-      "total-sales": "Общий объем продаж",
-      "top-products": "Лучшие товары",
-    },
-
-    "buttons": {
-      "all-channels": "Все каналы продаж",
-      "online-store": "Онлайн магазин",
-      "view-order": "Просмотреть заказ",
-      "view-orders": "Просмотреть заказы",
-      "create-button": "Создать кнопку",
-      "create-buy-button": "Создать кнопку корзины",
-      "learn-more": "Узнать больше"
+    'filters': {
+      'action': 'Filtrer',
+      'description': 'Montrer tous les items avec:',
     }
   },
 
-  "products-index": {
-    "table": {
-      "tabs": {
-        "all-products": "Все товары",
-      },
+  'home-index': {
+    'cards': {
+      'api_buy_button_create_first_embed_card': '',
+      'multi_image_buy_button_announcement_card': '',
     },
 
-    "buttons": {
-      "add-product": "Добавить товары",
-    },
-  },
-
-  "orders-index": {
-    "table": {
-      "tabs": {
-        "all-orders": "Все заказы",
-        "open": "Открыт",
-        "unfulfilled": "Ожидает обработки",
-        "unpaid": "Неоплачен",
-      },
-      "columns": {
-        "order": "Заказ",
-        "date": "Дата",
-        "customer": "Покупатель",
-        "payment-status": "Статус оплаты",
-        "fulfillment-status": "Статус отгрузки",
-        "total": "Итого",
-      },
-      "badges": {
-        "fulfilled": "Обработан",
-        "unfulfilled": "Ожидает обработки",
-        "paid": "Оплачено",
-        "pending": "Не обработан",
-        "refunded": "Возврат",
-        "authorized": "Утвержден"
-      },
+    'today-features': {
+      "todays-total-sales": "Total des ventes aujourd'hui",
+      "todays-visits": "Nombre de visites aujourd'hui",
+      "todays-orders": "Nombre de commandes aujourd'hui",
     },
 
-    "filters": {
-      "status": "Статус",
-      "credit-card": "Кредитная карта",
-      "customer": "Покупатель",
-      "payment-status": "Статус оплаты",
-      "fulfillment-status": "Статус отгрузки",
-      "tagged-with": "Теги",
-      "risk-level": "Уровень риска",
+    'titles': {
+      'total-sales': "Total des ventes",
+      'top-products': "Meilleurs produits",
     },
 
-    "buttons": {
-      "create-order": "Сделать заказ",
-    },
-  },
-
-  "orders-show": {
-    "buttons": {
-      "print": "Печать",
-      "cancel-order": "Отменить заказ",
-      "archive": "Архив",
-      "view-order-status-page": "Просмотр страницы заказов",
-      "capture-payment": "Оплата платежей",
-      "restock": "Пополнить",
-      "fulfill-items": "Реализовать",
-    },
-
-    "titles": {
-      "order-details": "Детали заказа",
-      "unfulfilled": "Ожидает обработки",
-      "accept-payment": "Принять оплату",
-      "fulfill-items": "Реализовать",
-      "risk-level": "Уровень риска"
-    },
-  },
-
-  "customers-show": {
-    "titles": {
-      "recent-orders": "Последние заказы",
-      "default-address": "Адрес по умолчанию",
-      "contact": "Контакты",
-    },
-  },
-
-  "draft-orders-index": {
-    "buttons": {
-      "create-order": "Создать заказ"
-    },
-    "empty": {
-      "title": "Наименование",
-      "subtitle": "Подзаголовок."
+    'buttons': {
+      'all-channels': "Tous les cannaux",
+      'online-store': "Boutique en ligne",
+      'view-order': "Voir la commande",
+      'view-orders': "Voir les commandes",
+      'create-button': "Créer un Buy Button",
+      'create-buy-button': "Créer un Buy Button",
+      'learn-more': "En apprendre plus"
     }
   },
 
-  "draft": {
-    "buttons": {
-      "save-draft-order": "Сохранить черновик"
+  'products-index': {
+    'table': {
+      'tabs': {
+        'all-products': "Tous les produits",
+      },
     },
 
-    "titles": {
-      "order-details": "Детали заказов",
-      "conversion-details": "Детали конверсии",
-      "unfulfilled": "Ожидает Обработки",
-      "accept-payment": "Принять оплату",
-      "fulfill-items": "Реализовать",
-      "risk-level": "Уровень риска",
-      "customer": "Покупатель",
-      "tags": "Тэги",
-      "order-contact": "Контакты заказчика",
-      "timeline": "Сроки",
-      "payment-pending": "Оплата ожидается",
+    'buttons': {
+      'add-product': "Ajouter un produit",
     },
-  }
+  },
+
+  'orders-index': {
+    'table': {
+      'tabs': {
+        'all-orders': "Toutes les commandes",
+        'open': "Ouvertes",
+        'unfulfilled': "Remplies",
+        'unpaid': "Non payées",
+      },
+
+      'columns': {
+        'order': 'Commande',
+        'date': 'Date',
+        'customer': 'Client',
+        'payment-status': 'Payment',
+        'fulfillment-status': 'Statut',
+        'total': 'Total',
+      },
+
+      'badges': {
+        'fulfilled': 'Complétée',
+        'unfulfilled': 'Non complétée',
+        'paid': 'Payée',
+        'pending': 'En attente',
+        'refunded': 'Remboursée',
+        'authorized': "Authorisée"
+      },
+    },
+
+    'filters': {
+      'select-a-filter...': 'Sélectionner un filtre',
+      'status': "Statut",
+      'credit-card': "Carte de crédit",
+      'chargeback-and-inquiry-status': "Rebondissements et enquêtes",
+      'customer': "Client",
+      'payment-status': "Paiement",
+      'sales-channel': "Canaux de vente",
+      'fulfillment-status': "Statut de complétion",
+      'tagged-with': "Avec tags...",
+      'risk-level': "Niveau de risque",
+    },
+
+    'buttons': {
+      'create-order': "Ajouter une commande",
+    },
+  },
+
+  'orders-show': {
+    'buttons': {
+      'print': "Imprimer",
+      'cancel-order': "Canceller",
+      'archive': "Archiver",
+      'view-order-status-page': "Voir la page de statut de la commande",
+      'capture-payment': "Capturer",
+      'restock': "Restaurer l'inventaire",
+      'fulfill-items': "Compléter",
+      'resend-email': "Réenvoyer le courriel",
+      'mark-as-paid': "Marquer comme payé",
+    },
+
+    'titles': {
+      'order-details': 'Détails',
+      'conversion-details': 'Détails de conversion',
+      'unfulfilled': 'Non complétés',
+      'accept-payment': 'Paiement',
+      'fulfill-items': 'Compléter',
+      'risk-level': 'Niveau de risque',
+      'customer': 'Client',
+      'tags': 'Tags',
+      'order-contact': 'Contact',
+      'timeline': 'Chronologie',
+      'payment-pending': 'Paiement en attente',
+    },
+
+    'texts': {
+      'show-comments': "Montrer les commentaires",
+    },
+  },
+
+  'checkouts-index': {
+    'table': {
+      'tabs': {
+        'all-checkouts': "Tous les checkouts",
+      },
+
+      'columns': {
+        'checkout': 'Commande',
+        'date': 'Date',
+        'placed-by': 'Client',
+        'email-status': 'Courriel',
+        'recovery-status': 'Statut',
+        'total': 'Total',
+      },
+
+      'badges': {
+        'sent': 'Envoyé',
+        'recovered': 'Récupéré',
+        'not-recovered': 'Non récupéré',
+        'not-sent': 'Non envoyé',
+      },
+    },
+  },
+
+  'checkouts-show': {
+    'buttons': {
+      'send-a-cart-recovery-email': 'Envoyer un courriel'
+    },
+  },
+
+  'customers-show': {
+    'titles': {
+      'recent-orders': "Commandes récentes",
+      'default-address': "Addresse principale",
+      'contact': "Information",
+    },
+  },
+
+  'draft-orders-index': {
+    'buttons': {
+      'create-order': 'Créer une commande'
+    },
+
+    'empty': {
+      'title': 'Créer des commandes et factures',
+      'subtitle': 'Acceptez des commandes au téléphone, envoyez des factures par courriel et acceptez des paiements.'
+    }
+  },
+
+  'draft': {
+    'buttons': {
+      'save-draft-order': 'Sauvegarder la commande',
+      'browse-products': "Parcourir les produits",
+      'add-custom-item': "Ajouter un article personnalisé",
+      'view-all-tags': "Voir tous les tags",
+      'add-discount': "Ajouter un rabais",
+      'add-shipping': "Ajouter du shipping",
+      'taxes': "Taxes",
+      'email-invoice': "Envoyer",
+      'mark-as-paid': "Marquer comme payé",
+      'mark-as-pending': "Marquer comme en attente",
+      'pay-with-credit-card': "Payer par carte de crédit",
+    },
+
+    'titles': {
+      'order-details': 'Détails',
+      'find-or-create-a-customer': 'Trouver ou créer un client',
+      'email-invoice': 'Envoyer la facture par courriel',
+      'accept-payment': 'Paiement',
+    },
+  },
 }
